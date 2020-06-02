7ac57d70bbf7ea103732e7f1e5a2655f4cac8800
Constantine Derkach <derkach@benefitty.ru>
2020-06-02 12:20:04 +0300

    fix

---
+++ database/migrations/2020_06_01_164943_create_locations_tabl.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTabl extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('directum_id');
+            $table->boolean('is_checked_out');
+            $table->float('lat');
+            $table->float('lng');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
